63deaff4bb22619178ed23a5bb374c10f0f1c816
Ri-Hong <65200215+Ri-Hong@users.noreply.github.com>
2021-01-10 14:12:25 -0500

    Add files via upload

---
+++ ESP01-MQTTGarageDoor/ESP01-MQTTGarageDoor.ino
@@ -0,0 +1,202 @@
+#include <ESP8266WiFi.h>
+#include "PubSubClient.h"
+
+//declare all the functions beforehand
+void handleRcvMsg( String sMsg );
+void setup_wifi();
+void publishAck(char *pAckMsg);
+void callback(char* topic, byte *payload, unsigned int length);
+void reconnect();
+
+int nextHeartbeatTime = 0; //Time in ms indicating when to send out next heartbeat. "heartbeat" is published regularly to show that this device is alive. Used in publishHeartbeat()
+unsigned long wakeUpTime = millis(); //any msg received before this time is ignored. Used in handleRcvMsg()
+
+// Update these with values suitable for your network.
+const char* ssid = "SAMPLE_WIFI_SSID"; //FILL IN WITH YOUR SSID
+const char* password = "SAMPLE_PASSWORD"; //FILL IN WITH YOUR NETWORK PASSWORD
+const char* mqtt_server = "broker.hivemq.com"; //You can choose to use another server
+#define mqtt_port 1883
+#define MQTT_USER "ESP01" //You can choose to change the user if you want
+#define MQTT_PASSWORD "" //leave blank if using a public broker
+#define MQTT_PUBLISH_TOPIC "foo/bar" // CHOOSE A TOPIC NAME. 
+#define MQTT_SUBSCRIBE_TOPIC "foo/bar" //SHOULD BE SAME AS MQTT_PUBLISH_TOPIC
+
+#define GPIO0_PIN 0 //Using a variable instead of a magic number 0
+
+// In this circuit design, when the GPIO0 is set to LOW, the relay will be closed.  
+// Therefore, the garage will be opened.
+// Also, the on-board LED will be ON when LED_BUILTIN is set to LOW.
+#define ON LOW 
+#define OFF HIGH
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+
+//------------------------------------------------------
+void setup() {
+  Serial.println("Beginning");
+  Serial.begin(115200);
+
+  //initialize all the pinmodes and set them off
+  pinMode(GPIO0_PIN, OUTPUT);
+  digitalWrite(GPIO0_PIN, OFF);
+  pinMode(LED_BUILTIN, OUTPUT); //Just a note that LED_BUILTIN is linked to GPIO2, so that is why you cannot use GPIO2 to control the relay.
+  digitalWrite(LED_BUILTIN, OFF); //Turn LED OFF
+  
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port); //setup mqtt server and port
+  client.setCallback(callback); //execture calback function when an incoming message is received
+  reconnect();
+}
+
+//------------------------------------------------------
+void loop() {
+  client.loop(); //Used to monitor the server and read incoming messages
+
+  publishHeartbeat();
+   
+  if(millis() < 10000){ //Used to deal with rollover of the millis() function
+    wakeUpTime = millis();
+  }
+  delay(1000);
+}
+
+
+//------------------------------------------------------
+//Function used to publish a message "heartbeat" onto the server every 10 seconds
+void publishHeartbeat(){
+  int heartbeatInterval = 10000; //the delay between each heartbeat message in milliseconds
+  reconnect(); //reconnect to the server if we are somehow disconnected
+  //nextHeartbeatTime stores the time at which the next heartbeat should be sent. If the current time is greater then the next heartbeat time, then a heartbeat will be published. It then updates nextHeartbeatTime and also flashes the built in LED on the ESP01
+  if(millis() > nextHeartbeatTime){ 
+    client.publish(MQTT_PUBLISH_TOPIC, "heartbeat");
+    nextHeartbeatTime += heartbeatInterval;
+    digitalWrite(LED_BUILTIN, ON); // Turn ON LED
+    delay(500);
+    digitalWrite(LED_BUILTIN, OFF);  //Turn OFF LED
+  }
+}
+
+
+//------------------------------------------------------
+//Used to publish an acknowledgement to a request to open the door
+void publishAck(char *pAckMsg){ 
+  if (!client.connected()) { //Will check if connected to server. If not, it will attempt to reconnect.
+    reconnect();
+  }
+  client.publish(MQTT_PUBLISH_TOPIC, pAckMsg);
+}
+
+//------------------------------------------------------
+//Used to setup wifi 
+void setup_wifi() {
+    delay(10);
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    randomSeed(micros());
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+//------------------------------------------------------
+//Used to reconnect to the MQTT server and topic if not already connected
+void reconnect() {
+  static int reconnectionAttempts = 0;
+  char pMsg[50];
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(),MQTT_USER,MQTT_PASSWORD)) {
+      Serial.println("connected");
+      //Once connected, publish an announcement...
+
+      sprintf( pMsg, "Hello from ESP: %d. Just connected", reconnectionAttempts++ );
+      client.publish(MQTT_PUBLISH_TOPIC, pMsg);
+      // ... and resubscribe
+      client.subscribe(MQTT_SUBSCRIBE_TOPIC);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+//------------------------------------------------------
+//Handles the first stage of incoming messages from the server
+// when this function is called, the following is passed to this function:
+// topic: the subscribed topic
+// payload: the content of the received msg
+// length: number of bytes in the msg
+void callback(char* topic, byte *payload, unsigned int length) {
+  char pPayload[50] = "";
+  
+  Serial.println("-------new message from broker-----");
+  Serial.print("channel:");
+  Serial.println(topic);
+  Serial.print("data:");  
+  Serial.write(payload, length);
+  Serial.println();
+
+  for( int i=0; i<length; i++ ) //convert the received bytes to chars
+  {
+    sprintf( pPayload, "%s%c", pPayload, payload[i] ); 
+  }
+
+  //Once a request to open the door is received, all other messages will be ignored for the miliseconds specified by msSleepTime found in handleRcvMsg()
+  if(millis() >= wakeUpTime){
+    handleRcvMsg( (String) pPayload );
+  }
+  else{
+    Serial.println("Sleeping, message ignored");
+  }
+}
+
+//------------------------------------------------------
+//Handles the second stage of incoming messages from the server
+void handleRcvMsg( String sMsg ){
+  int msSleepTime = 3000; //How long after each request will all incoming requests for the next n miliseconds be ignored. Currently 3 sec. Purpose is to avoid accidential multiple presses of the button in the app
+  Serial.println(sMsg);
+  if (sMsg == "38"){ //once code is received, GPIO0 will be pulled low, the relay will activate and the door will open. The LED will flash twice and a "1" will be published onto the server
+    Serial.println("Pin high");
+    digitalWrite(GPIO0_PIN, ON);
+    digitalWrite(LED_BUILTIN, ON);   // Turn ON LED
+    delay(500);
+    digitalWrite(LED_BUILTIN, OFF);   // Turn OFF LED
+    delay(500);
+    digitalWrite(LED_BUILTIN, ON);   // Turn ON LED
+    delay(500);
+    Serial.println("Pin low");
+    digitalWrite(GPIO0_PIN, OFF);
+    digitalWrite(LED_BUILTIN, OFF); //Turn OFF LED
+    publishAck("1");
+
+    wakeUpTime = millis() + msSleepTime; //update the time until which all further requests will be ignored
+    }
+  else if(sMsg == "1" or sMsg == "0" or sMsg == "heartbeat"){//self published message. No need to publish acknowledgement to server
+  }
+  else{ //for unrecognizable requests
+    Serial.print("Unrecognized code: ");
+    Serial.println(sMsg);
+    publishAck("0");
+  }
+
+}
+
+ 
---
+++ README.md
@@ -0,0 +1,36 @@
+# Garage-Door-Opener-Module-App
+## General Description
+The ESP01 Module is wired to the garage door opener. When the user taps a button on the app, the app sends a code via MQTT to the ESP01 Module, which then sends a one second pulse to the garage door opener, which opens the garage door. The ESP01 module must be connected to your home wifi network for the MQTT communication to work, however, the phone with the app can be connected to the internet in any way including using cellular data or wifi.
+
+## Description of ESP01 Module 
+The ESP01 Module is subscribed to a MQTT topic. When it receives a code via the companion app, the ESP01's GPIO0 pin will be set low for one second. GPIO0 being pulled low will activate the relay and will switch the normally open switch to closed. This will complete the circuit between the garage door's red terminal and it's white terminal, which will cause the door to open/close depending on the state that it is in. The module also has a asknowledgement feature. Once the code is received, the module will publish a "1" to the MQTT server to show the sender that it has successfully received their code. If the code is incorrect, the module will publish a "0". The module also has a heartbeat feature. Every 10 seconds, the module will publish "heartbeat" onto the server to show that it is still functioning.
+
+![Image of ESP01 module](Images/ESP01_Module.jpg "Image of ESP01 module")
+![Image of ESP01 module in box](Images/Module_In_Box.jpg "Image of ESP01 module in box")
+![Image of Garage Door Wiring](Images/Garage_Door_Wiring.jpg "Image of Garage Door Wiring")
+
+### Materials Required
+* ESP01
+* ESP01 Programming Adapter
+* 5V Relay
+* 2x 10k Resistors
+* PNP Transistor
+* A USB Power Brick
+* A few wires to connect everything together
+
+### Circuit Diagram
+![Image of Circuit Diagram](Images/Circuit_Diagram.jpg "Image of Circuit Diagram")
+
+### Notes
+* When programming the ESP01 Module using the ESP01 Programming Adapter, make sure to connect the GPIO0 pin to the Ground pin to set the ESP01 to programming mode. Then disconnect it after the program is done uploading. Also, the Enable Pin has to be wired to the VCC pin to keep the module operational.
+* You have to keep the ESP01 Module powered on for it to work. Since the ESP01 Programming Adapter has a USB connector, I just plugged it into a power brick and plugged the brick into an outlet beside the garge door opener. You can also choose to supply the ESP01 Module with 3.3V directly using the VCC pin and GND pin.
+* My specific garage door opener activates when the red terminal is connected to the white terminal. You may want to research your own garage door opener to see how it functions as it may differ from mines. The red and white terminals on the M1 on the circuit diagram refers to the red and white terminals on the garage door opener. For my garage door opener, when the red and white terminals are connected when the door is closed, the door will open and vice versa, if the terminals are connected when the door is opened, the door will close.
+* I handcrafted the wooden box that contains the entire module using popsicle sticks and wood glue. You may choose to use any container you want. 
+* The power brick is one that I had lying around the house. If you don't have one, they can be purchesed online for a few dollars.
+* The other wires connecting to the red and white terminals of the garage door opener are from a button switch in the garage which were already attatched to the opener before I added the wires from my module.
+
+## Description of Android Application
+The app requires the user to tap the "ready" button then tap the "go" button within 2 seconds. The two buttons are there to avoid accidental touches. The app then publishes a code onto the MQTT topic which can be read by the ESP01 module. The app will display the last time it received a heartbeat message from the ESP01 module as well as the current time accurate to the second. After sending a request to open the door to the ESP01 module, it will display "Success" if the ESP01 module recognizes the code, "Failed" if the module does not recognize the code, and the actual message itself if the response is not a "0" or "1".
+
+<img src="Images/App_Screenshot.png" alt="Screenshot of app interface" width="200"/>
+Feel free to contact me at riri.hong@gmail.com if you have any questions!
